221dca645392bc4133d5850dad0027a5cd5b82ac
apexdev <mwangi.em37@gmail.com>
2023-05-30 16:17:52 +0300

    refactor: Modularize logic
    Modularize the logic for the function: 'send_custom_greeting' and break function into smaller logical units.

--- wp-content/plugins/hello-rest-block/hello-rest-block.php
+++ wp-content/plugins/hello-rest-block/hello-rest-block.php
@@ -97,32 +97,37 @@ class Hello_REST_Block_REST_Client {
 	}
 
 	public function send_custom_greeting() {
+		$url = $this->build_custom_greeting_url();
+		$response = wp_remote_request($url, $this->get_request_args());
+		return is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200? 
+			$this->handle_rest_response($response) : $this->render_custom_greeting($response);
+	}
+
+	private function build_custom_greeting_url() {
 		$url = $this->base_url . $this->endpoint;
-		$args = array(
+		$params = array('greeting' => 'greetings-from-wp-client');
+		$url = add_query_arg($params, $url);
+		return $url; 
+	}
+
+	private function get_request_args() {
+		// Define your request args here
+		return [
 			'timeout' => 10,
 			'method' => 'GET',
 			'sslverify' => false
-		);	
+		];
+	}
 
-		$params = array('greeting' => 'hello-from-wp-client');
-		$final_url = add_query_arg($params, $url);
-		$response = wp_remote_request($final_url, $args);
+	private function handle_rest_response($response) {
+		$custom_error_message = 'Oops, couldn\'t send our friend a greeting :(';
+		$error_message = is_wp_error($response)? 
+			$response->get_error_message() : $custom_error_message;
+		return '<span>' . $error_message .  '</span>';
+	}
 
-		// Handle response 
-		// ...
-
-		if (is_wp_error($response)) {
-			$error_message = $response->get_error_message();
-			return '<span>' . $error_message .  '</span>';
-		}
-		
-		$response_code = wp_remote_retrieve_response_code($response);
-		if ($response_code !== 200) {
-			$custom_error_message = 'Oops, couldn\'t send our friend a greeting :(';
-			return '<span>' . $custom_error_message .  '</span>';
-		}
-
+	private function render_custom_greeting($response) {
 		$custom_greeting = wp_remote_retrieve_body($response);
 		return '<span>' . $custom_greeting .  '</span>';
 	}
-}
+ }
